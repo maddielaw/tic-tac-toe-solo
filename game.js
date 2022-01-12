660d56bde47ff9c504d456239d21ddf1a2d67633
Madison Law <madisonlaw1@gmail.com>
2022-01-12 16:26:48 -0700

    Add basic function to simulate a turn

--- game.js
+++ game.js
@@ -7,25 +7,10 @@ class Game {
                   "", "", "",
                   "", "", ""];
     this.currentPlayer = this.player1;
-    this.turn = 0;
   };
-  checkPlayerTurn() {
-    if (this.turn = 0) {
-      this.currentPlayer = this.player1
-    } else if (this.turn = 1) {
-      this.currentPlayer = this.player2
-    }
-    // if (this.currentPlayer === this.player1) {
-    //   this.currentPlayer = this.player2
-    // } else if (this.currentPlayer === this.player2) {
-    //   this.currentPlayer = this.player1
-    // }
+  playTurn(token, position) {
+    this.board[position] = token
     // WORKING IN TERMINAL
-  };
-  playTurn() {
-
-    // Player 1 always starts on page load. In each turn we need to return an object with the player object and the cell that was clicked on (could have it separated by cell # & column. Column being letter, and cell # being index)
-    // Will need to update this.board with correct indices based on the cell that was clicked on (cell 2C corresponds to index 2. cell 7B corresponds to index 7)
   };
   checkForEmpty() {
     if (this.board.includes("X") || this.board.includes("O")) {
@@ -43,10 +28,17 @@ class Game {
       }
     // WORKING IN TERMINAL
   };
+  updatePlayerWins() {
+    this.currentPlayer.wins ++
+  }
   checkWinningPlayer() {
-    if (this.turn === 0) {
+    if (this.currentPlayer === this.player1) {
+      this.updatePlayerWins();
+      this.currentPlayer = this.player2;
       return "player1 wins!"
-    } else if (this.turn === 1) {
+    } else if (this.currentPlayer === this.player2) {
+      this.updatePlayerWins();
+      this.currentPlayer = this.player1;
       return "player2 wins!"
     }
     // code to check who most recently played
@@ -56,28 +48,20 @@ class Game {
     var board = this.board;
     if (board[0] === board[1] && board[0] === board[2]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[3] === board[4] && board[3] === board[5]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[6] === board[7] && board[6] === board[8]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[0] === board[3] && board[0] === board[6]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[1] === board[4] && board[1] === board[7]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[2] === board[5] && board[2] === board[8]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[0] === board[4] && board[0] === board[8]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (board[2] === board[4] && board[2] === board[6]) {
         return this.checkWinningPlayer()
-        // return "It's a win!"
     } else if (!board.includes("")) {
         return "It's a draw!"
     }
@@ -100,6 +84,10 @@ class Game {
 /* 
 
 
+DRAW CONDITION EXAMPLE
+"X", "O", "O", "O", "X", "X", "X", "O", "O"
+
+
 WIN CONDTIONS
 var winningCombos = [
   [0, 1, 2],
@@ -115,12 +103,15 @@ var winningCombos = [
 
 
 When player takes a turn:
-- should know which player's turn it is
+- should know which player's turn it is (can do manually for now)
 - Should update the board with their selection (takes in argument of X or O & location?)
-- After each turn should check for win or draw condition
+- After each turn should run checkForWinOrDraw
+- should update this.turn & this.currentPlayer (currently doing that in checkWinningPlayer)
 - 
 
-
+playTurn(token, position) {
+  this.board[position] = token
+}
 
 
 
